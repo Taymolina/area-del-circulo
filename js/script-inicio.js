0cefaf92f7bf3f52e4530cc3cd37f085029db049
ivanna cuesta <tayana.molina@wissen.edu.ec>
2021-06-06 20:21:49 -0500

    circulo

---
+++ js/script-inicio.js
@@ -0,0 +1,20 @@
+<? PushSubscription
+if (isset ($_POST ["button"])) {
+    $radio=$_POST["radio"];
+
+
+    if (empty ($_POST[radio]]){
+
+    } else {
+
+        $total=($radio)"($radio)"*(3.14));
+        echo "</br>";
+        echo <h3> la medida del radio = $radio cm <br></h3>;
+        echo <h2> "El total del area= cm2<br></h2>";
+    }
+}
+function colocarSubtitulo(){
+    document.getElementById("lblSubtitulo").innerText = subtitulo;
+
+
+}
